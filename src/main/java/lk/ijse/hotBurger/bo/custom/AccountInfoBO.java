46f73393b624a7025a075fc0b8525743dabeca2d
LRLayan <rameshlayan4@gmail.com>
2023-12-28 01:33:19 +0530

    insert update username and password method

--- src/main/java/lk/ijse/hotBurger/bo/custom/AccountInfoBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/AccountInfoBO.java
@@ -2,5 +2,9 @@ package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
 
+import java.sql.SQLException;
+
 public interface AccountInfoBO extends SuperBO {
+    boolean updateUsername(String confirmUsername, int userId) throws SQLException;
+    boolean updateUserPassword(String confirmPassword, int userId) throws SQLException;
 }
