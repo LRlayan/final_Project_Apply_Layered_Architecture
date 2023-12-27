c93ae3ac9dde0377ded26de7290d2fce96f62b56
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:25:20 +0530

    create save,getAll,update and delete methods

--- src/main/java/lk/ijse/hotBurger/bo/custom/DeliveryBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/DeliveryBO.java
@@ -1,6 +1,15 @@
 package lk.ijse.hotBurger.bo.custom;
 
+import lk.ijse.hotBurger.Entity.DeliveryDetail;
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.DeliveryDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface DeliveryBO extends SuperBO {
+    boolean save(DeliveryDto entity) throws SQLException;
+    ArrayList<DeliveryDto> getAll() throws SQLException;
+    boolean update(DeliveryDto dto) throws SQLException;
+    boolean delete(String id) throws SQLException;
 }
