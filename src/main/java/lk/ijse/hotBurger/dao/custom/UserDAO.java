f22fe3c261f7bc613d72b02424ce1ad36b8c0c27
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:10:54 +0530

    loose coupling userDAOImpl class

--- src/main/java/lk/ijse/hotBurger/dao/custom/UserDAO.java
+++ src/main/java/lk/ijse/hotBurger/dao/custom/UserDAO.java
@@ -1,4 +1,11 @@
 package lk.ijse.hotBurger.dao.custom;
 
-public interface UserDAO {
+import lk.ijse.hotBurger.Entity.User;
+import lk.ijse.hotBurger.dao.CrudDAO;
+
+import java.sql.SQLException;
+
+public interface UserDAO extends CrudDAO<User> {
+    boolean updateUsername(String confirmUsername,int userId ) throws SQLException;
+    boolean updateUserPassword(String confirmPassword , int userId) throws SQLException;
 }
