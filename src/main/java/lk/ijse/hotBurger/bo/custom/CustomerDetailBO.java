ebc402b00ccc37a799bd1887777f77f9ff9e5af8
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:13:08 +0530

    getAll method

--- src/main/java/lk/ijse/hotBurger/bo/custom/CustomerDetailBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/CustomerDetailBO.java
@@ -1,6 +1,12 @@
 package lk.ijse.hotBurger.bo.custom;
 
+import lk.ijse.hotBurger.Entity.Customer;
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.CustomerDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface CustomerDetailBO extends SuperBO {
+    ArrayList<CustomerDto> getAll() throws SQLException;
 }
