8978904ffd9a1095b7326fadc675bf9fc87a9f86
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:24:32 +0530

    Override methods

--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/DeliveryBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/DeliveryBOImpl.java
@@ -1,6 +1,29 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
 import lk.ijse.hotBurger.bo.custom.DeliveryBO;
+import lk.ijse.hotBurger.dto.DeliveryDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DeliveryBOImpl implements DeliveryBO {
+    @Override
+    public boolean save(DeliveryDto entity) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public ArrayList<DeliveryDto> getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean update(DeliveryDto dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException {
+        return false;
+    }
 }
