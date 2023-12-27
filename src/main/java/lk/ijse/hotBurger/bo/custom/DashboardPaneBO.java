bcb6dcfd74ea29620df251d8e09a0b81bf3a692d
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:15:50 +0530

    total Sales Amount method

--- src/main/java/lk/ijse/hotBurger/bo/custom/DashboardPaneBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/DashboardPaneBO.java
@@ -1,6 +1,10 @@
 package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.OrderDto;
+
+import java.sql.SQLException;
 
 public interface DashboardPaneBO extends SuperBO {
+    OrderDto totalSalesAmount() throws SQLException;
 }
