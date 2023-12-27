3baf17051d666051c1729ca2eab1ce9451162bee
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:40:34 +0530

    dineAndPickUpCustomerSave method

--- src/main/java/lk/ijse/hotBurger/bo/custom/DineInBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/DineInBO.java
@@ -1,6 +1,11 @@
 package lk.ijse.hotBurger.bo.custom;
 
+import lk.ijse.hotBurger.Entity.Customer;
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.CustomerDto;
+
+import java.sql.SQLException;
 
 public interface DineInBO extends SuperBO {
+    CustomerDto dineAndPickUpCustomerSave(CustomerDto dto) throws SQLException;
 }
