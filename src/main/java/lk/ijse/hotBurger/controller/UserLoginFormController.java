667194e883a3813caddc4a56f510ac3b88696149
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:14:24 +0530

    renamed class

--- src/main/java/lk/ijse/hotBurger/controller/UserLoginFormController.java
+++ src/main/java/lk/ijse/hotBurger/controller/UserLoginFormController.java
@@ -6,15 +6,16 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.hotBurger.bo.BOFactory;
+import lk.ijse.hotBurger.bo.custom.UserBO;
 import lk.ijse.hotBurger.dto.UserDto;
-import lk.ijse.hotBurger.model.UserModel;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 
-public class AdminLoginFormController {
+public class UserLoginFormController {
     public AnchorPane loginAnchorPane;
 
     public javafx.scene.control.Label lblPassword;
@@ -27,19 +28,28 @@ public class AdminLoginFormController {
     @FXML
     private PasswordField txtPasswordField;
 
-    UserDto userDto = new UserDto();
-
     DuplicateMethodController navigate = new DuplicateMethodController();
+    UserBO userBO = (UserBO) BOFactory.getBoFactory().BOTypes(BOFactory.BOTypes.USER);
+
+    ArrayList<UserDto> allUsers;
+
+    {
+        try {
+            allUsers = userBO.getAll();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void ClickOnLogingBtn(ActionEvent actionEvent) throws IOException, SQLException {
         String username = txtUsername.getText();
         String password = txtPasswordField.getText();
-        ArrayList<UserDto> allUsers = UserModel.getAllUsers();
 
         for (int i = 0; i < allUsers.size(); i++) {
             if (username.equals(allUsers.get(i).getUsername()) && password.equals(allUsers.get(i).getPassword()) && allUsers.get(i).getType().equals("admin")) {
                 AccountInfoFormController.userId = allUsers.get(i).getId();
                 navigate.navigate("/view/mainDashboard_form.fxml", loginAnchorPane);
+
             } else if (username.equals(allUsers.get(i).getUsername()) && password.equals(allUsers.get(i).getPassword()) && allUsers.get(i).getType().equals("cashier")){
                 AccountInfoFormController.userId = allUsers.get(i).getId();
                 navigate.navigate("/view/CashierWork_form.fxml", loginAnchorPane);
@@ -48,14 +58,14 @@ public class AdminLoginFormController {
     }
 
     public void onEnterKeyUsername(KeyEvent keyEvent) throws IOException, SQLException {
-        ArrayList<UserDto> allUsers = UserModel.getAllUsers();
+        //ArrayList<UserDto> allUsers = UserModel.getAllUsers();
         String username = txtUsername.getText();
         navigate.incorrectCredential(allUsers , username , "Incorrect Username" , lblUsername);
     }
 
     @FXML
     void OnEnterPassword(KeyEvent event) throws SQLException {
-        ArrayList<UserDto> allUsers = UserModel.getAllUsers();
+        //ArrayList<UserDto> allUsers = UserModel.getAllUsers();
         String password = txtPasswordField.getText();
         navigate.incorrectCredential(allUsers , password , "Incorrect Password" , lblPassword);
     }
