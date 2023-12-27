062be762d66524a6a0d6bae81948d600cc1ecca1
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:40:49 +0530

    bug fix

--- src/main/java/lk/ijse/hotBurger/model/UserModel.java
+++ src/main/java/lk/ijse/hotBurger/model/UserModel.java
@@ -8,62 +8,44 @@ import java.util.ArrayList;
 
 public class UserModel {
 
-    UserDto userDto = new UserDto();
-
-    public static ArrayList<UserDto> getAllUsers() throws SQLException {
-        ArrayList<UserDto> arrayList = new ArrayList<>();
-        Connection connection = DbConnection.getInstance().getConnection();
-
-        String sql = "SELECT * from user";
-
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        while (resultSet.next()) {
-            UserDto userDto = new UserDto(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6));
-            arrayList.add(userDto);
-        }
-        return arrayList;
-    }
-
-//    public static ArrayList<UserDto> getUsernames() throws SQLException {
-//        ArrayList<UserDto> usernames = new ArrayList<>();
+//  public static ArrayList<UserDto> getAllUsers() throws SQLException {
+//        ArrayList<UserDto> arrayList = new ArrayList<>();
 //        Connection connection = DbConnection.getInstance().getConnection();
 //
-//        String sql = "SELECT username FROM user";
+//        String sql = "SELECT * from user";
+//
 //        PreparedStatement preparedStatement = connection.prepareStatement(sql);
 //
 //        ResultSet resultSet = preparedStatement.executeQuery();
-//        while (resultSet.next()){
-//            UserDto userDto = new UserDto(resultSet.getString(1));
-//            usernames.add(userDto);
+//
+//        while (resultSet.next()) {
+//            UserDto userDto = new UserDto(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6));
+//            arrayList.add(userDto);
 //        }
-//        return usernames;
+//        return arrayList;
+//        return null;
 //    }
-
-    public boolean updateUser(String confirmUsername,int userId ) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
-        String sql = "UPDATE user SET username = ? WHERE id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-       // System.out.println(confirmUsername  + "  "  + oldUsername);
-      preparedStatement.setString(1,confirmUsername);
-     // preparedStatement.setString(2,oldUsername);
-        preparedStatement.setInt(2,userId);
-
-        return preparedStatement.executeUpdate() > 0;
-    }
-
-    public boolean updateUserPassword(String confirmPassword , int userId) throws SQLException {
-
-        Connection connection = DbConnection.getInstance().getConnection();
-        String sql = "UPDATE user SET password = ? WHERE id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-        preparedStatement.setString( 1 , confirmPassword);
-        preparedStatement.setInt(2,userId);
-
-        return  preparedStatement.executeUpdate() > 0;
-    }
+//
+//    public boolean updateUser(String confirmUsername,int userId ) throws SQLException {
+//        Connection connection = DbConnection.getInstance().getConnection();
+//        String sql = "UPDATE user SET username = ? WHERE id = ?";
+//        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//
+//        preparedStatement.setString(1,confirmUsername);
+//        preparedStatement.setInt(2,userId);
+//
+//        return preparedStatement.executeUpdate() > 0;
+//    }
+//
+//    public boolean updateUserPassword(String confirmPassword , int userId) throws SQLException {
+//
+//        Connection connection = DbConnection.getInstance().getConnection();
+//        String sql = "UPDATE user SET password = ? WHERE id = ?";
+//        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//
+//        preparedStatement.setString( 1 , confirmPassword);
+//        preparedStatement.setInt(2,userId);
+//
+//        return  preparedStatement.executeUpdate() > 0;
+//    }
 }
