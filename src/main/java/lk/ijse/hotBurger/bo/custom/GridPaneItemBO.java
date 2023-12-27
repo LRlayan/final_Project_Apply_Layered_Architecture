8c99d06768b4fffef3099c99093dffd8de1528ee
LRLayan <rameshlayan4@gmail.com>
2023-12-28 03:19:03 +0530

    loadAllItemCategoryVise method override

--- src/main/java/lk/ijse/hotBurger/bo/custom/GridPaneItemBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/GridPaneItemBO.java
@@ -1,6 +1,12 @@
 package lk.ijse.hotBurger.bo.custom;
 
+import lk.ijse.hotBurger.Entity.Item;
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface GridPaneItemBO extends SuperBO {
+    ArrayList<ItemDto> loadAllItemCategoryVise(int catID) throws SQLException;
 }
--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/GridPaneItemBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/GridPaneItemBOImpl.java
@@ -1,6 +1,26 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
+import lk.ijse.hotBurger.Entity.Item;
 import lk.ijse.hotBurger.bo.custom.GridPaneItemBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.ItemDAO;
+import lk.ijse.hotBurger.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class GridPaneItemBOImpl implements GridPaneItemBO {
+
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
+    @Override
+    public ArrayList<ItemDto> loadAllItemCategoryVise(int catID) throws SQLException {
+        ArrayList<Item> allItems = itemDAO.loadAllItemCategoryVise(catID);
+
+        ArrayList<ItemDto> itemDto = new ArrayList<>();
+        for (Item items : allItems) {
+            itemDto.add(new ItemDto(items.getId(), items.getItemCode(), items.getName(), items.getUnitPrice(), items.getUnitCost(), items.getCategoryId(), null));
+
+        }
+        return itemDto;
+    }
 }
