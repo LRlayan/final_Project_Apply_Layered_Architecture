894d00429f6c52627e223fcb530d47333e303fd2
LRLayan <rameshlayan4@gmail.com>
2023-12-28 01:32:08 +0530

    username and password update method

--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/AccountInfoBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/AccountInfoBOImpl.java
@@ -1,6 +1,22 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
 import lk.ijse.hotBurger.bo.custom.AccountInfoBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.UserDAO;
+
+import java.sql.SQLException;
 
 public class AccountInfoBOImpl implements AccountInfoBO {
+
+    UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.USER);
+
+    @Override
+    public boolean updateUsername(String confirmUsername, int userId) throws SQLException {
+        return userDAO.updateUsername(confirmUsername, userId);
+    }
+
+    @Override
+    public boolean updateUserPassword(String confirmPassword, int userId) throws SQLException {
+        return userDAO.updateUserPassword(confirmPassword, userId);
+    }
 }
