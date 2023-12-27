e794a502241053588291361ebcc2d9d76c91d18f
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:16:25 +0530

    total Sales Amount method return dto

---
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/DashboardPaneBoImpl.java
@@ -0,0 +1,19 @@
+package lk.ijse.hotBurger.bo.custom.impl;
+
+import lk.ijse.hotBurger.Entity.Order;
+import lk.ijse.hotBurger.bo.custom.DashboardPaneBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.OrderDAO;
+import lk.ijse.hotBurger.dto.OrderDto;
+
+import java.sql.SQLException;
+
+public class DashboardPaneBoImpl implements DashboardPaneBO {
+
+    OrderDAO orderDAO = (OrderDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ORDER);
+    @Override
+    public OrderDto totalSalesAmount() throws SQLException {
+        Order totalSalesAmount = orderDAO.totalSalesAmount();
+        return new OrderDto(totalSalesAmount.getTotal());
+    }
+}
