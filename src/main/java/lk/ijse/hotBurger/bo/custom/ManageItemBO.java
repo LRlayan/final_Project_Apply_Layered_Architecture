493ba2000c8e4742cca0e246ff11e53e44b90b74
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:09:27 +0530

    item all method create

--- src/main/java/lk/ijse/hotBurger/bo/custom/ManageItemBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/ManageItemBO.java
@@ -1,6 +1,15 @@
 package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface ManageItemBO extends SuperBO {
+    ArrayList<ItemDto> getAllItem() throws SQLException;
+    ArrayList<ItemDto> loadAllItemCategoryVise(int catID) throws SQLException;
+    boolean updateItem(ItemDto itemDto) throws SQLException;
+    boolean deleteItem(String itemCode) throws SQLException;
 }
