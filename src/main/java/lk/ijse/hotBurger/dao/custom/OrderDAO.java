21342e0b21389e603e089ce674da474c6c79c266
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:11:43 +0530

    loose coupling OrderDAOImpl class

--- src/main/java/lk/ijse/hotBurger/dao/custom/OrderDAO.java
+++ src/main/java/lk/ijse/hotBurger/dao/custom/OrderDAO.java
@@ -1,4 +1,13 @@
 package lk.ijse.hotBurger.dao.custom;
 
-public interface OrderDAO {
+import lk.ijse.hotBurger.Entity.Order;
+import lk.ijse.hotBurger.dao.CrudDAO;
+import lk.ijse.hotBurger.dto.OrderDto;
+
+import java.sql.SQLException;
+
+public interface OrderDAO extends CrudDAO<Order> {
+    Order totalSalesAmount() throws SQLException;
+    int totalSales() throws SQLException;
+    Order profit() throws SQLException;
 }
