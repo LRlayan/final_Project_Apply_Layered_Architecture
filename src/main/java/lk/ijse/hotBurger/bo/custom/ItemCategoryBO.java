191df0e45d727d7e82d9f00cdd30fb4223e58652
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:07:50 +0530

    item category all method create

--- src/main/java/lk/ijse/hotBurger/bo/custom/ItemCategoryBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/ItemCategoryBO.java
@@ -1,6 +1,12 @@
 package lk.ijse.hotBurger.bo.custom;
 
+import lk.ijse.hotBurger.Entity.ItemCategory;
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.ItemCategoryDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface ItemCategoryBO extends SuperBO {
+    ArrayList<ItemCategoryDto> getAllItemCategory() throws SQLException;
 }
