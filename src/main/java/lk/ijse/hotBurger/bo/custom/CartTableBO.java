ecb1b8c30c263fecffe442c8d5b0c267475e6634
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:06:11 +0530

    place order all method create

--- src/main/java/lk/ijse/hotBurger/bo/custom/CartTableBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/CartTableBO.java
@@ -1,6 +1,20 @@
 package lk.ijse.hotBurger.bo.custom;
 
+import lk.ijse.hotBurger.Entity.Customer;
+import lk.ijse.hotBurger.Entity.Order;
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.CustomerDto;
+import lk.ijse.hotBurger.dto.DeliveryDto;
+import lk.ijse.hotBurger.dto.OrderDetailsDto;
+import lk.ijse.hotBurger.dto.OrderDto;
+
+import java.sql.SQLException;
 
 public interface CartTableBO extends SuperBO {
+
+    boolean saveOrder(OrderDto orderDto) throws SQLException;
+    boolean saveCustomer(CustomerDto customerDto) throws SQLException;
+    boolean saveDelivery(DeliveryDto deliveryDto) throws SQLException;
+    boolean saveOrderDetail(OrderDetailsDto orderDetailsDto) throws SQLException;
+    boolean dineAndPickUpCustomerSave(CustomerDto customerDto) throws SQLException;
 }
