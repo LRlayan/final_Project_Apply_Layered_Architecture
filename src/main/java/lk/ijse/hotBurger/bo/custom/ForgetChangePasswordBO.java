36a172e1cd72a13c6186c56d19ad33cfa714a7d2
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:46:03 +0530

    updateUserPassword method

--- src/main/java/lk/ijse/hotBurger/bo/custom/ForgetChangePasswordBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/ForgetChangePasswordBO.java
@@ -2,5 +2,7 @@ package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
 
+import java.sql.SQLException;
+
 public interface ForgetChangePasswordBO extends SuperBO {
-}
+    boolean updateUserPassword(String confirmPassword, int userId) throws SQLException;}
