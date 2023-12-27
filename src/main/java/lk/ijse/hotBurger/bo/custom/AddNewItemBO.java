d65edbe84825d9c7038db80d55a43fe673254ffa
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:05:38 +0530

    add item all method create

--- src/main/java/lk/ijse/hotBurger/bo/custom/AddNewItemBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/AddNewItemBO.java
@@ -1,6 +1,10 @@
 package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.AddNewItemDto;
+
+import java.sql.SQLException;
 
 public interface AddNewItemBO extends SuperBO {
+    boolean addNewItem(AddNewItemDto newItemDto) throws SQLException;
 }
