f4b44c72674aa1df2d5b5ed1e99342fa02c4cecf
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:40:22 +0530

    dineAndPickUpCustomerSave method

--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/DineInBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/DineInBOImpl.java
@@ -1,6 +1,14 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
+import lk.ijse.hotBurger.Entity.Customer;
 import lk.ijse.hotBurger.bo.custom.DineInBO;
+import lk.ijse.hotBurger.dto.CustomerDto;
+
+import java.sql.SQLException;
 
 public class DineInBOImpl implements DineInBO {
+    @Override
+    public CustomerDto dineAndPickUpCustomerSave(CustomerDto dto) throws SQLException {
+        return null;
+    }
 }
