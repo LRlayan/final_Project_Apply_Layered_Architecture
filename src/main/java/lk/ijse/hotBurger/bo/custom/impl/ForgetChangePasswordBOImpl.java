1aef17ed020e22c6d6af43c09232d97d7758fc6f
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:46:17 +0530

    updateUserPassword method Override

--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/ForgetChangePasswordBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/ForgetChangePasswordBOImpl.java
@@ -1,6 +1,17 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
+import lk.ijse.hotBurger.bo.custom.AccountInfoBO;
 import lk.ijse.hotBurger.bo.custom.ForgetChangePasswordBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.UserDAO;
+
+import java.sql.SQLException;
 
 public class ForgetChangePasswordBOImpl implements ForgetChangePasswordBO {
+
+    UserDAO userDAO = (UserDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.USER);
+    @Override
+    public boolean updateUserPassword(String confirmPassword, int userId) throws SQLException {
+        return userDAO.updateUserPassword(confirmPassword, userId);
+    }
 }
