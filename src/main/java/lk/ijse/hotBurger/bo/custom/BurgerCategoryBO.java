3ee9d87925906b2e18f93eeee094a3fe0c25591e
LRLayan <rameshlayan4@gmail.com>
2023-12-28 01:34:30 +0530

    load AllItem Category Vise

--- src/main/java/lk/ijse/hotBurger/bo/custom/BurgerCategoryBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/BurgerCategoryBO.java
@@ -1,6 +1,11 @@
 package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface BurgerCategoryBO extends SuperBO {
+    ArrayList<ItemDto> loadAllItemCategoryVise(int catID) throws SQLException;
 }
--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/BurgerCategoryBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/BurgerCategoryBOImpl.java
@@ -1,6 +1,26 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
+import lk.ijse.hotBurger.Entity.Item;
 import lk.ijse.hotBurger.bo.custom.BurgerCategoryBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.ItemDAO;
+import lk.ijse.hotBurger.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class BurgerCategoryBOImpl implements BurgerCategoryBO {
+
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
+
+    @Override
+    public ArrayList<ItemDto> loadAllItemCategoryVise(int catID) throws SQLException {
+        ArrayList<Item> allItem = itemDAO.loadAllItemCategoryVise(catID);
+        ArrayList<ItemDto> itemDto = new ArrayList<>();
+
+        for (Item items : allItem) {
+            itemDto.add(new ItemDto(items.getCategoryId(),items.getItemCode(),items.getName(),items.getUnitPrice(),items.getUnitCost(),items.getId()));
+        }
+        return itemDto;
+    }
 }
