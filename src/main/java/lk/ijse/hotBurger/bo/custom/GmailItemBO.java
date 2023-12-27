475ef9f70bb5a0fb470e90a35e54d1cb921b5b2f
LRLayan <rameshlayan4@gmail.com>
2023-12-28 03:18:22 +0530

    getALlItem method

--- src/main/java/lk/ijse/hotBurger/bo/custom/GmailItemBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/GmailItemBO.java
@@ -1,6 +1,11 @@
 package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface GmailItemBO extends SuperBO {
+    ArrayList<ItemDto> getAllItem() throws SQLException;
 }
