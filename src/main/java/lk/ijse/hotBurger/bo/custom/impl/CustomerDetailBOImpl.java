c42ee0217a66f383aa130e681150ed203b54c568
LRLayan <rameshlayan4@gmail.com>
2023-12-28 02:13:32 +0530

    getAll method implementation

--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/CustomerDetailBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/CustomerDetailBOImpl.java
@@ -1,6 +1,26 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
+import lk.ijse.hotBurger.Entity.Customer;
 import lk.ijse.hotBurger.bo.custom.CustomerDetailBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.CustomerDAO;
+import lk.ijse.hotBurger.dto.CustomerDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CustomerDetailBOImpl implements CustomerDetailBO {
+
+    CustomerDAO customerDAO = (CustomerDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.CUSTOMER);
+    @Override
+    public ArrayList<CustomerDto> getAll() throws SQLException {
+        ArrayList<Customer> getAllCustomer = customerDAO.getAll();
+        ArrayList<CustomerDto> customerDto = new ArrayList<>();
+
+        for (Customer customers : getAllCustomer) {
+            customerDto.add(new CustomerDto(customers.getId(), customers.getFName(), customers.getLName(), customers.getMobile() , customers.getAddress()));
+        }
+
+        return customerDto;
+    }
 }
