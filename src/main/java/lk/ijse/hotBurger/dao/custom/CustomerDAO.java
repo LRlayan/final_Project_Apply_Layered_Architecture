3e29ade3bfb59df8826e7856f3daba6629de7f49
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:13:39 +0530

    loose coupling CustomerDAOImpl class

--- src/main/java/lk/ijse/hotBurger/dao/custom/CustomerDAO.java
+++ src/main/java/lk/ijse/hotBurger/dao/custom/CustomerDAO.java
@@ -1,4 +1,11 @@
 package lk.ijse.hotBurger.dao.custom;
 
-public interface CustomerDAO {
+import lk.ijse.hotBurger.Entity.Customer;
+import lk.ijse.hotBurger.dao.CrudDAO;
+import lk.ijse.hotBurger.dto.CustomerDto;
+
+import java.sql.SQLException;
+
+public interface CustomerDAO extends CrudDAO<Customer> {
+    Customer dineAndPickUpCustomerSave(Customer entity) throws SQLException;
 }
