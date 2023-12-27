7b7a6464a6bbdd8c788649abed9bbee238b6843f
LRLayan <rameshlayan4@gmail.com>
2023-12-27 21:41:42 +0530

    item detail entity

---
+++ src/main/java/lk/ijse/hotBurger/Entity/Item.java
@@ -0,0 +1,23 @@
+package lk.ijse.hotBurger.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Item {
+    private int id;
+    private String itemCode;
+    private String name;
+    private double unitPrice;
+    private double unitCost;
+    private String  categoryId;
+    private String image;
+
+    public Item(int id, String itemCode, String name, double unitPrice, double unitCost, String categoryId) {
+
+    }
+}
