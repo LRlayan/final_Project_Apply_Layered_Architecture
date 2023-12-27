ca6e276f29c259bd30482f11534045702316f285
LRLayan <rameshlayan4@gmail.com>
2023-12-28 03:18:36 +0530

    getALlItem method override

--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/GmailItemBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/GmailItemBOImpl.java
@@ -1,6 +1,25 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
+import lk.ijse.hotBurger.Entity.Item;
 import lk.ijse.hotBurger.bo.custom.GmailItemBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.ItemDAO;
+import lk.ijse.hotBurger.dto.ItemDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class GmailItemBOImpl implements GmailItemBO {
+
+    ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
+    @Override
+    public ArrayList<ItemDto> getAllItem() throws SQLException {
+      ArrayList<Item> allItem = itemDAO.getAll();
+      ArrayList<ItemDto> itemDto = new ArrayList<>();
+
+        for (Item items : allItem) {
+            itemDto.add(new ItemDto(items.getCategoryId(),items.getItemCode(),items.getName(),items.getUnitPrice(),items.getUnitCost(),items.getId()));
+        }
+        return itemDto;
+    }
 }
