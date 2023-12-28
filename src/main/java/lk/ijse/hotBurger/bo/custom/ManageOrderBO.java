bae090ab55ae0f82f7eb00935c857fb03e0f984c
LRLayan <rameshlayan4@gmail.com>
2023-12-28 13:52:55 +0530

    loadAllOrders() method

--- src/main/java/lk/ijse/hotBurger/bo/custom/ManageOrderBO.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/ManageOrderBO.java
@@ -1,6 +1,13 @@
 package lk.ijse.hotBurger.bo.custom;
 
 import lk.ijse.hotBurger.bo.SuperBO;
+import lk.ijse.hotBurger.dto.OrderDto;
+
+import java.sql.Array;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface ManageOrderBO extends SuperBO {
+    ArrayList<OrderDto> loadAllOrders() throws SQLException;
 }
