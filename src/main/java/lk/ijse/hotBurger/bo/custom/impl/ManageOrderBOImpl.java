089d9f3055be44f646936b63d4f831aec3bdbb96
LRLayan <rameshlayan4@gmail.com>
2023-12-28 13:53:10 +0530

    loadAllOrders() method Override

--- src/main/java/lk/ijse/hotBurger/bo/custom/impl/ManageOrderBOImpl.java
+++ src/main/java/lk/ijse/hotBurger/bo/custom/impl/ManageOrderBOImpl.java
@@ -1,6 +1,25 @@
 package lk.ijse.hotBurger.bo.custom.impl;
 
+import lk.ijse.hotBurger.Entity.Order;
 import lk.ijse.hotBurger.bo.custom.ManageOrderBO;
+import lk.ijse.hotBurger.dao.DAOFactory;
+import lk.ijse.hotBurger.dao.custom.OrderDAO;
+import lk.ijse.hotBurger.dto.OrderDto;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ManageOrderBOImpl implements ManageOrderBO {
+
+    OrderDAO orderDAO = (OrderDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ORDER);
+    @Override
+    public ArrayList<OrderDto> loadAllOrders() throws SQLException {
+        ArrayList<Order> getAllOrder = orderDAO.getAll();
+        ArrayList<OrderDto> orderDto = new ArrayList<>();
+
+        for(Order orders : getAllOrder){
+            orderDto.add(new OrderDto(orders.getId(),orders.getSubTotal(),orders.getDeliveryCharge(),orders.getDiscount(),orders.getDate(),orders.getTotal(),orders.getCustomerId(),orders.getType()));
+        }
+        return orderDto;
+    }
 }
